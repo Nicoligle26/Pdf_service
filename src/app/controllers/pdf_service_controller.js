a2e581ea4ad16674cfe6ded33713b3bb0a732a12
Nicolle Iglesias Covo <90627846+Nicoligle26@users.noreply.github.com>
2022-01-28 17:10:05 -0500

    Controller pdf service

--- src/app/controllers/pdf_service_controller.js
+++ src/app/controllers/pdf_service_controller.js
@@ -1,14 +1,13 @@
 const tmp = require('tmp-promise');
 const AWS = require('aws-sdk');
-const fs = require('fs').promises;
+const carbone = require('carbone');
 
-const PdfService = require('../services/pdf_service');
-const { getTemplatesOpts, getTemplate } = require('../schemas/templates');
+const { getTemplate } = require('../schemas/templates');
 
-async function create(req, reply) {
+async function create(_req, reply) {
   AWS.config.update({ region: 'us-east-1' });
 
-  s3 = new AWS.S3({});
+  const s3 = new AWS.S3({});
   const t = await tmp.file();
   console.log(t);
 
@@ -16,16 +15,52 @@ async function create(req, reply) {
     Bucket: 'guarapo-pdf-service',
     Key: 'templates/prueba2.odt',
   };
-  try {
-    const readStream = s3.getObject(params).createReadStream();
 
-    const file = await fs.writeFile(t.path, readStream, { flag: 'w+' });
-    console.log(file);
+  const options = {
+    convertTo: 'pdf',
+  };
+
+  const data = {
+    users: [
+      {
+        number: '105083512',
+        name: 'John Barranco',
+        loan: '$600.000,00 M.C.',
+        balance: '$300.000,00 M.C.',
+        payment: '$300.000,00 M.C.',
+        interests: '$350.000,00 M.C.',
+        balanceDue: '$0',
+        coutum: 'No. cuota',
+        monthlyCoutum: '$175.000,00 M.C.',
+        minValue: '$175.000,00 M.C.',
+      },
+      {
+        number: '147038802',
+        name: 'John Barranco',
+        loan: '$600.000,00 M.C.',
+        balance: '$300.000,00 M.C.',
+        payment: '$300.000,00 M.C.',
+        interests: '$350.000,00 M.C.',
+        balanceDue: '$0',
+        coutum: 'No. cuota',
+        monthlyCoutum: '$175.000,00 M.C.',
+        minValue: '$175.000,00 M.C.',
+      },
+    ],
+  };
+
+  try {
+    s3.getObject(params).createReadStream();
+
+    carbone.render(t.path, data, options, function callback(_err, result) {
+      reply
+        .header('Content-Type', 'application/pdf')
+        .header('Content-Disposition', 'inline; filename="mypdf.pdf"')
+        .send(result);
+    });
   } catch (e) {
     console.log(e);
   }
-
-  reply.send({});
 }
 
 module.exports = async (fastify) => {
